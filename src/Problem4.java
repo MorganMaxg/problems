7a76ab2eef322372a04efe850ae13dbec75e7d9b
maxiaogang <maxiaogang@youzan.com>
2018-02-24 12:02:23 +0800

    添加第四道题目

---
+++ src/Problem4.java
@@ -0,0 +1,12 @@
+public class Problem4 {
+
+  public static void main(String[] args) {
+    /*
+     * 题目:if的使用
+     * 题目内容:判断一个数是奇数还是偶数,如果是偶数则输出"xx是偶数",如果是奇数则输出"xx是奇数"
+     * 结果验证:
+     * 输入:4 输出:4是偶数
+     * 输入:15 输出:15是奇数
+     */
+  }
+}
