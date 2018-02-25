00fbf325d430bf6089442201fccb70dd7d78e0fe
maxiaogang <maxiaogang@youzan.com>
2018-02-25 08:10:52 +0800

    添加第五道题目

---
+++ src/Problem5.java
@@ -0,0 +1,20 @@
+public class Problem5 {
+
+  public static void main(String[] args) {
+    /*
+     * 题目:比较大小
+     * 题目内容:给定两个整数A，B(1≤A,B≤1000)，请判断两数的关系。
+     * 输入:
+     * 第一行整数T表示测试数据组数。接下来T行每行两个整数分别表示A，B。 如果A<B输出Smaller，如果A=B输出Equal，如果A>B输出Bigger。
+        输入例子:
+        3
+        4 5
+        3 3
+        4 3
+        输出例子:
+        Smaller
+        Equal
+        Bigger
+     */
+  }
+}
